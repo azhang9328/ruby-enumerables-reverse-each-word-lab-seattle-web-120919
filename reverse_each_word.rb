94471dcca51175c297bee0f78398994c9b6e3e26
Allen Zhang <azhang9328@gmail.com>
2019-12-06 00:53:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,5 +8,5 @@ end
 
 oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", "Doc", "Lemonjello", "Sleepy" ]
 oppressed_workers.each do |oppressed_worker|
-   puts "#{oppressed_worker}"
+   puts "#{oppressed_worker.reverse}"
 end
