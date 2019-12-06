81d7889dcece080f74daa4647ea1e2fbb15fd42d
Allen Zhang <azhang9328@gmail.com>
2019-12-06 00:55:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentarray = []
-  sentarray.push(sentence.split(' ').to_s)
+  sentarray.push(sentence.split(' '))
   sentarray.each do |sentarray|
     sentarray.reverse
   end   
