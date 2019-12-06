311583316213001bc9e9a65f6edc5ab0da9eb896
Allen Zhang <azhang9328@gmail.com>
2019-12-06 00:49:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,8 @@ def reverse_each_word(sentence)
     puts "#{sentarray}"
   end   
 end   
+
+oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", "Doc", "Lemonjello", "Sleepy" ]
+oppressed_workers.each do |oppressed_worker|
+   puts "#{oppressed_worker}"
+end
